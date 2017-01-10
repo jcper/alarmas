e535a1cab8fece48b178621b6eba17efe2fbe93a
Your Name <you@example.com>
2017-01-10 03:02:25 +0100

    user+geolocalizacionip

--- AlarmaCpu.js
+++ AlarmaCpu.js
@@ -11,20 +11,30 @@ var i=0;
 var EmisorEventos = eventos.EventEmitter;
 var ee=new EmisorEventos();
 var readline = require('readline');
-var limite=0.2;//carga cpu.
+var limite=0.70;//carga cpu.
 var limiteRam=1073741824;//1 Gb memoria libre
 var dateAlarm=new Date().toTimeString();
 var tiempo=10000;//cada minuto lanza el evento.
 var WebSocket= require('ws');
 var url='wss://agile-citadel-80189.herokuapp.com/';
 var ws = new WebSocket(url);
-var user='equipo1';//nombre de usuario
+//var myip = require('quick-local-ip');
+var publicIp = require('public-ip');
 var alarma='';
 var AlarmStatus=false;
 var myjson='';
-
+var user=os.hostname();//nombre de usuario
+var ip_publica=publicIp.v4().then(ip => {
+    console.log(ip);
+    ip_publica=ip;
+    console.log(ip_publica);
+    alarma={"name":user,"alarma":0,"date":new Date().toTimeString(),"ip":ip_publica};
+  
+});
+ 
  
  function Vigilante(limite){
+
 if(limite!==0.01 && limiteRam!==0.01){
 
 if(os.loadavg()[1]>limite){
@@ -34,7 +44,7 @@ console.log('Tiempo [ms]: '+os.uptime());
 console.log('Memoria Total: ' + os.totalmem());
 console.log('Memoria Libre: ' + os.freemem());
  AlarmStatus=true// se produce alarma.
- alarma={"name":user,"alarma":1, "date":new Date().toTimeString()};
+ alarma={"name":user,"alarma":1, "date":new Date().toTimeString(),"ip":ip_publica,};
 
  if(i!==10){
  	var data=alarma;
@@ -71,7 +81,7 @@ console.log('Tiempo [ms]: '+os.uptime());
 console.log('Memoria Total: ' + os.totalmem());
 console.log('Memoria Libre: ' + os.freemem());
 AlarmStatus=true// se produce alarma.
-alarma={"name":user,"alarma":2, "date":new Date().toTimeString()};
+alarma={"name":user,"alarma":2, "date":new Date().toTimeString(),"ip":ip_publica};
 
 if(i!==10){
  	var data=alarma;
@@ -106,47 +116,27 @@ if(i!==10){
 if(AlarmStatus && os.freemem()<limiteRam && os.loadavg()[1]<limite){
 
     AlarmStatus=false;//Se vuelve a restablecer el sistema sin alarmas
-   alarma={"name":user,"alarma":0, "date":new Date().toTimeString()}//valores iniciales
+   alarma={"name":user,"alarma":0,"date":new Date().toTimeString(),"ip":ip_publica}//valores iniciales
 }
 
  //si no hay alarma
  ee.on('datos', function Vigilante(limite){
  console.log('Alarma testeando' + AlarmStatus);
-  });
+ });
 
   if(!AlarmStatus){
-alarma={"name":user,"alarma":0, "date":new Date().toTimeString()};
+alarma={"name":user,"alarma":0,"date":new Date().toTimeString(),"ip":ip_publica};
+console.log('ip'+ip_publica);
 ws.on('message', function(message) {
 myjson=JSON.stringify(alarma);
   ws.send(myjson);
-   console.log('cliente: %s', message); 
-    });
-  }
-  if(AlarmStatus && os.loadavg()[1]>limite ){
-  console.log('Alarma 1'); 	
- alarma={"name":user,"alarma":1, "date":new Date().toTimeString()};
-ws.on('message', function(message) {
-  myjson=JSON.stringify(alarma);
-  ws.send(myjson);
-  console.log('cliente: %s', message); 
-    });
-  }
-   if(AlarmStatus && os.freemem()>limiteRam ){
-   	console.log('Alarma 2'); 	
- alarma={"name":user,"alarma":2, "date":new Date().toTimeString()};
-ws.on('message', function(message) {
- myjson=JSON.stringify(alarma);
-  ws.send(myjson);
-   console.log('cliente: %s', message);
+   myjson='';
+   console.log('cliente1: %s', message); 
     });
   }
  
 
-   
-     
- 
-
- //Lanzamos el emisor de eventos
+  //Lanzamos el emisor de eventos
 setInterval(function(){
  ee.emit('datos',Vigilante(limite));
   }, tiempo);
